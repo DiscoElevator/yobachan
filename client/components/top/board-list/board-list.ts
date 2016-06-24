0e6b81f75c9e1cee6cf99c929e6bbbbc39acb270
DiscoElevator <maxdoom@mail.ru>
2016-06-24 15:14:11 +0300

    relative paths

--- client/components/top/board-list/board-list.ts
+++ client/components/top/board-list/board-list.ts
@@ -1,9 +1,10 @@
 import {Component} from "@angular/core";
 
 @Component({
+	moduleId: module.id,
 	selector: "board-list",
-	templateUrl: "client/components/top/board-list/board-list.html",
-	styleUrls: ["client/components/top/board-list/board-list.css"]
+	templateUrl: "board-list.html",
+	styleUrls: ["board-list.css"]
 })
 export class BoardList {
 	boards = [{
--- client/components/top/top.ts
+++ client/components/top/top.ts
@@ -2,9 +2,10 @@ import {Component} from "@angular/core";
 import {BoardList} from "./board-list/board-list";
 
 @Component({
+	moduleId: module.id,
 	selector: "top",
-	templateUrl: "client/components/top/top.html",
-	styleUrls: ["client/components/top/top.css"],
+	templateUrl: "top.html",
+	styleUrls: ["top.css"],
 	directives: [BoardList]
 })
 export class Top {};
