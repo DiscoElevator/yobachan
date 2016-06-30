476637f54e79f98b241856bb136b284fb6872ecb
DiscoElevator <maxdoom@mail.ru>
2016-06-30 10:19:20 +0300

    router navigation on page reload fix

--- client/app.ts
+++ client/app.ts
@@ -1,7 +1,6 @@
 import {Component, ViewEncapsulation} from "@angular/core";
 import { HTTP_PROVIDERS } from '@angular/http';
 import { ROUTER_DIRECTIVES } from '@angular/router';
-import { APP_ROUTER_PROVIDERS } from './routes';
 import {BoardService} from "./services/board-service"
 import {Top} from "./components/top/top";
 import {Board} from "./components/board/board";
@@ -11,7 +10,7 @@ import {Dashboard} from "./components/dashboard/dashboard";
 	encapsulation: ViewEncapsulation.None,
 	selector: "app",
 	directives: [ROUTER_DIRECTIVES, Top, Board, Dashboard],
-	providers: [HTTP_PROVIDERS, APP_ROUTER_PROVIDERS, BoardService],
+	providers: [HTTP_PROVIDERS, BoardService],
 	template: `<top></top>
 <router-outlet></router-outlet>
 <style>
--- client/main.ts
+++ client/main.ts
@@ -2,6 +2,7 @@ import "reflect-metadata";
 import "rxjs/Rx";
 
 import { bootstrap }    from '@angular/platform-browser-dynamic';
+import { APP_ROUTER_PROVIDERS } from './routes';
 import { App } from './app';
 
-bootstrap(App);
+bootstrap(App, [APP_ROUTER_PROVIDERS]);
