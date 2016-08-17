708c7082650a69a59a95f1fbf4babefec5aca0a1
DiscoElevator <maxdoom@mail.ru>
2016-08-17 15:17:05 +0300

    angular updated to RC5 (forms)

--- client/app-module.ts
+++ client/app-module.ts
@@ -1,13 +1,14 @@
 import {NgModule} from "@angular/core";
 import {BrowserModule} from "@angular/platform-browser";
 import {RouterModule} from "@angular/router";
+import {FormsModule} from "@angular/forms";
 
 import {App} from "./app";
 import {routes, appRoutingProviders} from './routes';
 
 @NgModule({
 	declarations: [App],
-	imports: [BrowserModule, routes],
+	imports: [BrowserModule, routes, FormsModule],
 	bootstrap: [App],
 	providers: [appRoutingProviders]
 })
